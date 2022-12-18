fb35a4c406935cb87f6ea09808626eadb31eac59
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-12-18 01:47:51 +0000

    Update location buttons component

--- src/components/LocationButtons.jsx
+++ src/components/LocationButtons.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
 
-const LocationButtons = ({ state, locations, changeLocation }) => {
+const LocationButtons = ({ changeLocation }) => {
+  const { locations } = useContext(AppContext);
+  
+  const changeLocation = (e) => {
+    const { name, textContent } = e.target;
+
+    setState((prevState) => ({ ...prevState, [name]: textContent }));
+  };
+
   return (
-    <section className='location-buttons-section'>
-      <div className='location-buttons'>
+    <section className="location-buttons-section">
+      <div className="location-buttons">
         {locations.map((location, index) => (
           <button
-            className='btn'
-            name='location'
+            className="btn"
+            name="location"
             onClick={changeLocation}
-            key={index}>
+            key={index}
+          >
             {location}
           </button>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LocationButtons
+export default LocationButtons;
