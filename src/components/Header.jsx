f2bfdddabeb2bf51735dd2afa820f4022905263a
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-12-18 01:48:15 +0000

    Update header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../context";
 
 const Header = () => {
+  const { state } = useContext(Context);
+
   return (
-    <header className='header'>
+    <header className="header">
       <h1>
-        myBarc<span className='material-symbols-outlined'>adjust</span>de
+        myBarc<span className="material-symbols-outlined">adjust</span>de
+        Generator
       </h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
