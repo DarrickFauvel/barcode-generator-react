b09e93edb5b3850a7284cc12bd6ceea6ae213ae1
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-12-18 01:45:59 +0000

    Create footer link list

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,4 +4,29 @@ import { Context } from "../context";
 const Footer = () => {
   const { state } = useContext(Context);
 
-export default Footer
+  return (
+    <footer className="footer">
+      <hr />
+      <p>Created by {state.author}.</p>
+      <ul className="footer__list">
+        <li>
+          <a className="footer__link" href={state.urlWebsite} target="_blank">
+            darrickfauvel.com
+          </a>
+        </li>
+        <li>
+          <a className="footer__link" href={state.urlLinkedIn} target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a className="footer__link" href={state.urlGitHub} target="_blank">
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
