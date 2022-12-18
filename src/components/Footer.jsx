dc2522d089f2eb13ef6ba5edfa7ab6f532b2bad4
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-12-18 03:24:19 +0000

    Add container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,27 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <hr />
-      <p>Created by {state.author}.</p>
-      <ul className="footer__list">
-        <li>
-          <a className="footer__link" href={state.urlWebsite} target="_blank">
-            darrickfauvel.com
-          </a>
-        </li>
-        <li>
-          <a className="footer__link" href={state.urlLinkedIn} target="_blank">
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a className="footer__link" href={state.urlGitHub} target="_blank">
-            GitHub
-          </a>
-        </li>
-      </ul>
+      <div className="container">
+        <hr />
+        <p>Created by {state.author}.</p>
+        <ul className="footer__list">
+          <li>
+            <a className="footer__link" href={state.urlWebsite} target="_blank">
+              darrickfauvel.com
+            </a>
+          </li>
+          <li>
+            <a className="footer__link" href={state.urlLinkedIn} target="_blank">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a className="footer__link" href={state.urlGitHub} target="_blank">
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
