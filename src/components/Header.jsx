45fde0a67275cc87525da6de6d0fb3062d88c74a
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-12-31 17:45:13 +0000

    Add navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import React, { useContext } from "react";
-import { Context } from "../context";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Context } from '../context';
 
 const Header = () => {
   const { state } = useContext(Context);
 
   return (
-    <header className="header">
-      <div className="container">
-      
+    <header className='header'>
+      <div className='container'>
         <h1>
-          myTools
+          <Link to='/'>myTools</Link>
         </h1>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to='generator'>barcode generator</Link>
+            </li>
+            <li>
+              <Link to='grocery'>grocery codes</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
