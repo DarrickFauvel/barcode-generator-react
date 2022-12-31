abb08143f16f10de3e21a0ac1e16cdcffd3dddfd
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-12-31 20:56:08 +0000

    Disable jsbarcode

--- src/components/Barcode.jsx
+++ src/components/Barcode.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { Context } from "../context";
 
-import ReactJsBarcode from "react-jsbarcode";
+// import ReactJsBarcode from "react-jsbarcode";
 
 const Barcode = (props) => {
   const { state } = useContext(Context);
 
-  return <ReactJsBarcode value={props.barcodeValue || state.barcodeValue} options={{ height: 50 }} />;
+  // return <ReactJsBarcode value={props.barcodeValue || state.barcodeValue} options={{ height: 50 }} />;
 };
 
 export default Barcode;
