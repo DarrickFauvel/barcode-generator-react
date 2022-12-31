f1f1c9d1e1612fa57d856f62803fdcc0410b13ab
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2022-12-31 17:43:43 +0000

    Add props capability

--- src/components/Barcode.jsx
+++ src/components/Barcode.jsx
@@ -3,10 +3,10 @@ import { Context } from "../context";
 
 import ReactJsBarcode from "react-jsbarcode";
 
-const Barcode = () => {
+const Barcode = (props) => {
   const { state } = useContext(Context);
 
-  return <ReactJsBarcode value={state.barcodeValue} options={{ height: 50 }} />;
+  return <ReactJsBarcode value={props.barcodeValue || state.barcodeValue} options={{ height: 50 }} />;
 };
 
 export default Barcode;
