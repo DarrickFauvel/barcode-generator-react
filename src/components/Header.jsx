c1b5e25fb6ba487ee0684586e7bbcaa9000150c1
Darrick Fauvel <darrick.fauvel@gmail.com>
2023-01-16 21:40:44 -0500

    Update app title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className='container'>
         <nav className='nav'>
           <div className='navbrand'>
-            <Link to='about'>redTools</Link>
+            <Link to='about'>redTool</Link>
           </div>
 
           <a href='#' onClick={() => isMenuOpen.set(!$isMenuOpen)}>
